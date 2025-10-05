7fd9e9b1964dd0ceb5afa49fec7c828f5abc61a8
1218hiyokoW2 <192060988+1218hiyokoW2@users.noreply.github.com>
2025-10-05 23:12:01 +0900

    ボタンデザイン一括

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 'use strict';
 
 // index.html 用
-const startImg = document.querySelector('.start-img');
+const startBtn = document.querySelector('.start-btn');
 const popup = document.getElementById('popup');
 const closeBtn = document.querySelector('.close');
 const agreeBtn = document.getElementById('agreeBtn');
 
 // index.html 用イベント
-if (startImg && popup && closeBtn && agreeBtn) {
-  startImg.addEventListener('click', (e) => {
+if (startBtn && popup && closeBtn && agreeBtn) {
+  startBtn.addEventListener('click', (e) => {
     e.preventDefault();
     popup.style.display = 'flex';
   });
