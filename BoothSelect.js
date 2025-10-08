c2408532607496c020d588742d3c60768aa93921
imanomura <206794822+imanomura@users.noreply.github.com>
2025-10-08 13:45:52 +0900

    削除

--- BoothSelect.js
+++
@@ -1,9 +0,0 @@
-// 'use strict';
-
-// //ドロップダウン
-// const drop_select = document.querySelector('select');
-// drop_select.addEventListener('change', () => {});
-
-// //ラジオボタン
-// const radio_select = document.querySelector('select');
-// radio_select.addEventListener('change', () => {});
